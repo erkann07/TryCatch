57e3cbd37d795bfdc3c1533b22e6a62ddd704a5a
Erkan <erkankrgz0707@gmail.com>
2025-06-23 13:02:23 +0300

    Add files via upload

---
+++ TryCatch.cs
@@ -0,0 +1,20 @@
+﻿
+bool input = false;
+int kareAl(int sayi)
+{
+    return sayi * sayi;
+}
+while (!input)
+{
+    try
+    {
+        Console.Write("Bir sayı giriniz:");
+        int sayi = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Sayınızın karesi: " + kareAl(sayi));
+        input = true;
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine("Hatalı veri girdiniz lütfen bir sayı giriniz.");
+    }
+}
